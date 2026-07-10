fe16b5682dc2187b59a12c96e4b96a417b6920f3
Junaid Ali <junaid.ali@techno-soft.com>
2026-07-10 17:32:14 +0500

    netlify changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebase: {
+    apiKey: "AIzaSyC6uJO-ShnZQYmJ53NHfyCDitvi2xFIqEc",
+    authDomain: "task-management-864e2.firebaseapp.com",
+    projectId: "task-management-864e2",
+    storageBucket: "task-management-864e2.firebasestorage.app",
+    messagingSenderId: "606235775146",
+    appId: "1:606235775146:web:a5089d95784deedd7d40d3"
+  }
 };
